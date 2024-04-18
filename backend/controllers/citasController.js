38e3c0b89716a1491f5196a81e037bb678615778
cesar <cesarrivera2622@gmail.com>
2024-04-18 16:16:19 -0500

    coneccion a mongo OK

--- backend/controllers/citasController.js
+++ backend/controllers/citasController.js
@@ -1,26 +1,36 @@
-const getCitas = (req, res) => {
+const asyncHandler = require('express-async-handler') //paquete que indica cual es el error sin tener que usar try,catch
+
+
+const getCitas = asyncHandler(async(req, res) => {
     res.status(200).json({
         message: 'get citas'
     })
-}
+})
 
-const createCitas = (req, res) => {
+const createCitas = asyncHandler( async(req, res) => {
+    
+    if(!req.body.fecha) {
+        res.status(400)
+        throw new Error('por favor especifica una fecha')
+    }
+
+
     res.status(201).json({
         message: 'crear cita'
     })
-}
+})
 
-const updateCitas = (req, res) => {
+const updateCitas = asyncHandler( async(req, res) => {
     res.status(200).json({
         message: `modificar la tarea con id ${req.params.id}`
     })
-}
+})
 
-const deleteCitas = (req, res) => {
+const deleteCitas = asyncHandler( async(req, res) => {
     res.status(200).json({
         message: `id: ${req.params.id}`
     })
-}
+})
 
 module.exports = {
     getCitas,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
+const connectDb = require('./config/db')
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
+connectDb()
+
 const app = express() //para inicializar la aplicacion
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //cuando en postman use /api/citas buscara en routes y citasroutes
 app.use('/api/citas', require('./routes/citasRoutes')) 
 
+app.use(errorHandler) //le digo al app que estoy usando manejador de errores
+ 
 app.listen(port, () => console.log(`servidor inciado en el puerto ${port}`))
